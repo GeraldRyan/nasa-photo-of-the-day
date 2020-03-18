0d27ed9946f1b5ac6afeaa2a5c504f25c97aa421
Webbuildermn <gerald.ryan40@yahoo.com>
2020-03-18 13:55:47 -0500

    Got image to appear, not sure best practice but it works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
 import "./App.css";
+import axios from 'axios'
 
-// GET https://api.nasa.gov/planetary/apod  //-- Nasa API
+axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)  //-- Nasa API
+  .then(function (response) {
+    
+    console.log("Hello Demo")
+    console.log(response)
+    const davey = x(response)
+    document.getElementById('root').appendChild(davey)
+    })
+    .catch(
+      console.log("Hello Planet")
+    )
+
+const x = (obj) => {
+
+  const davey = document.createElement('div')
+  const image = document.createElement('img')
+  image.setAttribute('src', obj.data.url)
+  davey.appendChild(image)
+  return davey
+}
 
 
-
-function App() {
+function App()
+{
   return (
     <div className="App">
       <p>
